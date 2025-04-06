41882d97f51bf34be6622c17e56a1a210e834394
feargalr <feargalr@gmail.com>
2025-04-06 09:46:07 +0930

    Add unit tests using testthat; validate TaxSEA returns expected structure with test data

---
+++ tests/testthat.R
@@ -0,0 +1,3 @@
+library(testthat)
+library(TaxSEA)
+test_check("TaxSEA")
---
+++ tests/testthat/test-TaxSEA.R
@@ -0,0 +1,12 @@
+test_that("TaxSEA returns valid output with test data", {
+  data("TaxSEA_test_data", package = "TaxSEA")
+  res <- TaxSEA(TaxSEA_test_data)
+
+  expect_type(res, "list")
+  expect_named(res, c("Metabolite_producers", "Health_associations", "BugSigDB"))
+
+  for (df in res) {
+    expect_s3_class(df, "data.frame")
+    expect_true(all(c("taxonSetName", "median_rank_of_set_members", "PValue", "Test_statistic", "FDR") %in% colnames(df)))
+  }
+})
